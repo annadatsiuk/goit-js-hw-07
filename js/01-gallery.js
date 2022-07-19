55a1298dc80cbbf81933927e8705cb910c2418bf
Anna Datsiuk <anna.datsiuk94@gmail.com>
2022-07-19 23:35:20 +0300

    final version

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,71 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const list = document.querySelector(".gallery");
+
+const listItemsMarkup = createListItemsMarkup(galleryItems);
+list.innerHTML = listItemsMarkup;
+
+function createListItemsMarkup(items) {
+  return items
+    .map(
+      (item) =>
+        `<div class="gallery__item">
+            <a class="gallery__link" href="${item.original}">
+                <img
+                    class="gallery__image"
+                    src="${item.preview}"
+                    data-source="${item.original}"
+                    alt="${item.description}"
+                />
+            </a>
+        </div>`
+    )
+    .join("");
+}
+
+list.addEventListener("click", showFullImage);
+
+function showFullImage(event) {
+  event.preventDefault();
+
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+
+  modalShow(event.target.dataset.source);
+}
+
+let instance;
+
+function modalShow(src) {
+  instance = basicLightbox.create(
+    `<div class="modal">
+         <img src="${src}" style="height:100vh; display:block"></img>
+        </div>`,
+    {
+      onShow: (instance) => {
+        addListener();
+      },
+      onClose: (instance) => {
+        removeListener();
+      },
+    }
+  );
+  instance.show();
+}
+function addListener() {
+  window.addEventListener("keydown", onEscapeBtn);
+}
+
+function onEscapeBtn(event) {
+  if (event.code === "Escape") {
+    instance.close();
+  }
+}
+
+function removeListener() {
+  window.removeEventListener("keydown", onEscapeBtn);
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,37 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+// Створення і рендер розмітки на підставі масиву даних galleryItems і наданого шаблону елемента галереї. Використовуй готовий код з першого завдання.
+const list = document.querySelector(".gallery");
+
+const listItemsMarkup = createListItemsMarkup(galleryItems);
+list.innerHTML = listItemsMarkup;
+
+function createListItemsMarkup(items) {
+  return items
+    .map(
+      (item) =>
+        `
+            <a class="gallery__link" href="${item.original}">
+                <img
+                    class="gallery__image"
+                    src="${item.preview}"
+                    data-source="${item.original}"
+                    alt="${item.description}"
+                />
+            </a>`
+    )
+    .join("");
+}
+
+// Ініціалізація бібліотеки після створення і додання елементів галереї у div.gallery.
+// Для цього ознайомся з документацією SimpleLightbox - насамперед секції «Usage» і «Markup».
+
+let lightbox = new SimpleLightbox('.gallery a', {
+    captions: true,
+    captionsData: "alt",
+    captionDelay: 250,
+
+});
